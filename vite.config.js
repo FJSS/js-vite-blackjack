1a0ab2635ff134b3f3526e4334b4497dc91871f6
FJSS <franciscojavier.sanchezsanchez@emeal.nttdata.com>
2023-12-05 16:52:58 +0100

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/js-vite-blackjack/', // Directorio base donde se van a buscar los ficheros de la WEB. 
+    plugins: []
+})
